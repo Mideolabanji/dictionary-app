9aa3ea873ff10d967d13337dd9378087afaa747e
Mideolabanji <olabanjikaosara@yahoo.com>
2022-03-15 01:42:08 +0100

    cleaned the code and included the input form component

---
+++ src/InputForm.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Form() {
+  return (
+    <div>
+      <form>
+        <input type="search" placeholder="Type any word..." />
+      </form>
+    </div>
+  );
+}
