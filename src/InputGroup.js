3956d6ee0164bd1910f680b82716c22d068d45b6
Mideolabanji <olabanjikaosara@yahoo.com>
2022-03-16 00:08:47 +0100

    included state to update value of input

--- src/InputGroup.js
+++ src/InputGroup.js
@@ -1,11 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Body from "./Body";
 
 export default function InputGroup() {
+  let [keyword, setKeyword] = useState(null);
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    alert(`Searching for ${keyword}`);
+  }
+
+  function updateKeywordDetails(event) {
+    setKeyword(event.target.value);
+  }
+
   return (
     <div className="InputGroup">
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
+          onChange={updateKeywordDetails}
           type="search"
           placeholder="Search for a word..."
           className="mb-5"
